167d6ed714c347505358a1d493ca223202a482f9
cleeryy <clery.arqueferradou@gmail.com>
2025-03-07 12:25:08 +0100

    [...] not the middleware in my case...

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
 
-const PUBLIC_FILE = /\.(.*)$/;
-
-export async function middleware(req: NextRequest) {
-  if (
-    req.nextUrl.pathname.startsWith("/_next") ||
-    req.nextUrl.pathname.includes("/api/") ||
-    PUBLIC_FILE.test(req.nextUrl.pathname)
-  ) {
-    return;
-  }
-
-  if (req.nextUrl.locale === "default") {
-    const locale = "en";
-    return NextResponse.redirect(
-      new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
-    );
-  }
-}
-export const config = {
-  matcher: ["/((?!ws|api|_next/static|_next/image|favicon.ico).*)"],
-};
+export const { auth: middleware } = NextAuth(authConfig);
