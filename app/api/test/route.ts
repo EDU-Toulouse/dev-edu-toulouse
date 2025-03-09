1092bb8f263299a93577cad23f639a4adb0839b6
cleeryy <clery.arqueferradou@gmail.com>
2025-03-09 17:56:12 +0100

    [?] i don't even know what am i doing rn

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,14 +1,19 @@
+// @/app/api/test/route.ts
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
     const session = await auth();
-    if (session) return NextResponse.json("Success");
-    return NextResponse.json({
-      status: 401,
-      data: "You must be logged in.",
-    });
+
+    if (session) {
+      return NextResponse.json({ status: 200, data: "Success" });
+    }
+
+    return NextResponse.json(
+      { status: 401, data: "You must be logged in." },
+      { status: 401 }
+    );
   } catch (error) {
     console.error("Error fetching events:", error);
     return NextResponse.json(
